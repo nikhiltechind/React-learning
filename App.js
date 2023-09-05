1b855157f970f6dc8d214b9b003500aecf04f29a
nikhiltechind <nikhilkumar91621@gmail.com>
2023-09-05 22:45:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+const heading = React.createElement("h1",{id:"hii"},"hello world ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
