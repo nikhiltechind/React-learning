c8bb01abf343c371efa76bc6093d8bbb25e86dc6
nikhiltechind <nikhilkumar91621@gmail.com>
2023-09-07 17:36:17 +0530

    update

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 
-const heading = React.createElement("h1",{id:"hii"},"hello world ");
+// const heading = React.createElement("h1",{id:"hii"},"hello world ");
+const jsxHeading = (
+<h1> className="head" 
+    React learning series
+</h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
