0ff78a340a0fa86399911b80150269a3f8bb3708
nikhiltechind <nikhilkumar91621@gmail.com>
2023-09-10 02:42:09 +0530

    component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
- const Header = () => {
+
+const APIdata = {
+  id: 2,
+  name: "Nikhil",
+  age :23
+}
+
+const Header = () => {
     return( 
      <div className="NavBar"> 
-       <h1>hello</h1>
+       <h1>Name : {APIdata.name}</h1>
        <h1>hello</h1>
        <h1>hello</h1>
        <h1>hello</h1>
